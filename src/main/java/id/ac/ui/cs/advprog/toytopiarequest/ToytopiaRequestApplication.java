8003e46fc408ca40fc8e31a39bdebca191bab096
fariskafedira <fariska.fedira@gmail.com>
2024-04-17 16:19:32 +0700

    initializing repo for sistem request barang use case

---
+++ src/main/java/id/ac/ui/cs/advprog/toytopiarequest/ToytopiaRequestApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.toytopiarequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToytopiaRequestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToytopiaRequestApplication.class, args);
+    }
+
+}
