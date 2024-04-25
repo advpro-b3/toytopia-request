a8be7c09073dfd9bee59af2a9e59c60785bfaedd
fariskafedira <fariska.fedira@gmail.com>
2024-04-25 18:02:47 +0700

    Implement Request model

--- src/main/java/id/ac/ui/cs/advprog/toytopiarequest/model/Request.java
+++ src/main/java/id/ac/ui/cs/advprog/toytopiarequest/model/Request.java
@@ -1,4 +1,29 @@
 package id.ac.ui.cs.advprog.toytopiarequest.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
 public class Request {
+    String requestId;
+    String productName;
+    String imageLink;
+    Double price;
+    String productLink;
+    String currency;
+    String status;
+
+
+    public Request(String requestId, String productName, String imageLink, Double price, String productLink, String currency, String status) {
+        this.requestId = requestId;
+        this.productName = productName;
+        this.imageLink = imageLink;
+        this.price = price;
+        this.productLink = productLink;
+        this.currency = currency;
+        this.status = status;
+    }
 }
--- src/test/java/id/ac/ui/cs/advprog/toytopiarequest/model/RequestTest.java
+++ src/test/java/id/ac/ui/cs/advprog/toytopiarequest/model/RequestTest.java
@@ -9,24 +9,24 @@ public class RequestTest {
 
     @BeforeEach
     void setUp() {
-        request = new Request();
-        request.setRequestId("eb558e9f-1c39-460e-8860-71afbaf63bd6");
-        request.setProductName("Toytopia XYZ");
-        request.setImageLink("http://example.com/image.png");
-        request.setPrice(100000);
-        request.setProductLink("http://example.com/product");
-        request.setCurrency("IDR");
-        request.setStatus("PENDING");
-    }
+        request = Request.builder()
+                .requestId("eb558e9f-1c39-460e-8860-71afbaf63bd6")
+                .productName("Toytopia XYZ")
+                .imageLink("http://example.com/image.png")
+                .price(100000.0)
+                .productLink("http://example.com/product")
+                .currency("IDR")
+                .status("PENDING")
+                .build();
     }
 
     @Test
     void testCreateRequest() {
-        assertEquals(eb558e9f-1c39-460e-8860-71afbaf63bd6, request.getRequestId());
+        assertEquals("eb558e9f-1c39-460e-8860-71afbaf63bd6", request.getRequestId());
         assertEquals("Toytopia XYZ", request.getProductName());
-        assertEquals("https://example.com/image.jpg", request.getImageLink());
-        assertEquals(100000, request.getPrice());
-        assertEquals("https://example.com/product", request.getProductLink());
+        assertEquals("http://example.com/image.png", request.getImageLink());
+        assertEquals(100000.0, request.getPrice());
+        assertEquals("http://example.com/product", request.getProductLink());
         assertEquals("IDR", request.getCurrency());
         assertEquals("PENDING", request.getStatus());
     }
