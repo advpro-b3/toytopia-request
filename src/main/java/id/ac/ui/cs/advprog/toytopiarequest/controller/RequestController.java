20b89ccd6049878ae0e5281f396637421aca80c9
fariskafedira <fariska.fedira@gmail.com>
2024-05-26 13:37:47 +0700

    add crossorigin in controller

--- src/main/java/id/ac/ui/cs/advprog/toytopiarequest/controller/RequestController.java
+++ src/main/java/id/ac/ui/cs/advprog/toytopiarequest/controller/RequestController.java
@@ -11,6 +11,7 @@ import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/requests")
+@CrossOrigin("*")
 public class RequestController {
 
     private final RequestService requestService;
